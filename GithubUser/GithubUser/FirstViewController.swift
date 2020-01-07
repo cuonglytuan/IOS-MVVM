3610d95bab7d9bf0eb163c89d73a4ec456c3f345
Silent Man <>
2020-01-07 12:58:20 +0700

    - init project

---
+++ GithubUser/GithubUser/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GithubUser
+//
+//  Created by リーツアン クオン on 1/7/20.
+//  Copyright © 2020 リーツアン クオン. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GithubUser/GithubUser/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GithubUser
+//
+//  Created by リーツアン クオン on 1/7/20.
+//  Copyright © 2020 リーツアン クオン. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
